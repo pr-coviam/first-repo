5c90b311c01a37acc0f65d4bdc122e81a7cf498e
pr-coviam <partha.roy@coviam.com>
2016-02-09 11:03:55 +0530

    Secondary Commit - Adding the wordcloud.

---
+++ wordcloud.R
@@ -0,0 +1,61 @@
+#Word Cloud Example
+
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+func<-function(){
+	filePath <- c("test.txt")
+text <- readLines(filePath)
+
+# Load the data as a corpus
+docs <- Corpus(VectorSource(text))
+	
+#inspect(docs)
+	
+#Replacing “/”, “@” and “|” with space
+	toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+	docs <- tm_map(docs, toSpace, "/")
+	docs <- tm_map(docs, toSpace, "@")
+	docs <- tm_map(docs, toSpace, "\\|")
+	
+# Convert the text to lower case
+docs <- tm_map(docs, content_transformer(tolower))
+
+# Remove numbers
+docs <- tm_map(docs, removeNumbers)
+
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+
+# Remove your own stop word
+# specify your stopwords as a character vector
+docs <- tm_map(docs, removeWords, c("blabla1", "blabla2")) 
+
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+
+# Text stemming
+# docs <- tm_map(docs, stemDocument)
+
+
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+
+head(d, 10)
+png("wordcloud.png",height=1080,width=720)
+
+set.seed(1234)
+#for having padding around the text -> scale=c(8,.2) .. 
+
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+dev.off()
+
+}
